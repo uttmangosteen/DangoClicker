544b94194a2424b813b4210dee1a57ede263fb0a
uttmangosteen <noisimportantpoint@gmail.com>
2023-03-06 18:11:55 +0900

    あとはデータベースだけじゃあああ

--- src/main/java/io/github/uttmangosteen/dangoclicker/DataBase.java
+++ src/main/java/io/github/uttmangosteen/dangoclicker/DataBase.java
@@ -2,22 +2,18 @@ package io.github.uttmangosteen.dangoclicker;
 
 import org.bukkit.Bukkit;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class MySQL {
-    public String host, database, username, password, port;
+public class DataBase {
     public void mySQL(){
-        host = Global.config.getString("mysql.host");
-        database = Global.config.getString("mysql.database");
-        username = Global.config.getString("mysql.username");
-        password = Global.config.getString("mysql.password");
-        port = Global.config.getString("mysql.port");
         Connect();
     }
-
     private boolean Connect(){
+        String host = Global.config.getString("mysql.host");
+        String database = Global.config.getString("mysql.database");
+        String username = Global.config.getString("mysql.username");
+        String password = Global.config.getString("mysql.password");
+        String port = Global.config.getString("mysql.port");
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
